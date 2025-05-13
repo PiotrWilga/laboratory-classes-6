faa491d4708f55b9ff82eb1c5de469d4820e6c15
piotrwilga <p_wilga@o2.pl>
2025-05-13 02:16:13 +0200

    Poprawiony problem z błędem powitalnym

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,5 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
-
-require(path.join(__dirname, '../server.js'));
+const waitOn = require('wait-on');
 
 let win;
 
@@ -15,17 +13,29 @@ function createWindow() {
     }
   });
 
+  // Ładujemy stronę dopiero po tym, jak serwer Express będzie dostępny
   win.loadURL('http://localhost:3000');
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  // Oczekiwanie na dostępność serwera na porcie 3000
+  waitOn({
+    resources: ['http://localhost:3000'],
+    timeout: 30000, // Czas oczekiwania na serwer (np. 30 sekund)
+  })
+    .then(() => {
+      createWindow(); // Po uzyskaniu dostępu do serwera, otwórz okno Electron
+    })
+    .catch((error) => {
+      console.error('Błąd oczekiwania na serwer:', error);
+      app.quit(); // Zakończ aplikację, jeśli nie uda się połączyć z serwerem
+    });
+});
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
 
 app.on('window-all-closed', () => {
